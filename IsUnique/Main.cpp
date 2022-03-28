f1d28ba7b6b51c8b31ae02747b15b2df6470bc56
Oreyato <oreyato.pro@gmail.com>
2022-03-28 16:18:56 +0200

    isUnique O(n) solution

--- IsUnique/Main.cpp
+++ IsUnique/Main.cpp
@@ -2,7 +2,8 @@
 #include <string>
 using namespace std;
 
-bool isUnique(string word) {
+// O(n^2) -bad- solution
+bool isUniqueN2(string word) {
 	for (int i = 0; i < word.length(); i++)
 	{
 		for (int j = 1; j < word.length(); j++)
@@ -18,6 +19,27 @@ bool isUnique(string word) {
 	return true;
 }
 
+// O(n) solution
+bool isUniqueN(string str) {
+	// Return false if there is more than 128 characters (there is bound to be a double in this case)
+	if (str.length() > 128) return false;
+
+	bool alreadyExists[128];
+	memset(alreadyExists, false, 128);
+
+	for (int i = 0; i < str.length(); i++)
+	{
+		int v = str[i];
+
+		if (alreadyExists[v]) {
+			return false;
+		}
+
+		alreadyExists[v] = true;
+	}
+
+	return true;
+}
 
 int main() {
 	string word;
@@ -26,7 +48,7 @@ int main() {
 	cin >> word;
 
 	bool areLettersUnique;
-	areLettersUnique = isUnique(word);
+	areLettersUnique = isUniqueN(word);
 	if (areLettersUnique) {
 		cout << "Yeh";
 	}
